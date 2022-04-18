be9978c58923424d973709b03df6f7ad801abc45
muhamadisro27 <mohammadisro2710@gmail.com>
2022-04-18 22:05:17 +0800

    feat : setup service get

---
+++ app/Http/Services/MatchService.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+namespace App\Http\Services;
+
+class MatchService {
+   
+   public function __construct()
+   {
+
+   }
+
+   public function index()
+   {
+      return [
+         'match' => 'parent'
+      ];
+   }
+
+}
+
+?>
