962c244cc60346d2dc549905eb3e88839212cbe3
muhamadisro27 <mohammadisro2710@gmail.com>
2022-04-18 21:41:58 +0800

    feat : setup resource route match string

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MatchStrController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/match-strings', MatchStrController::class);
