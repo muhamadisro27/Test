754714e934b31304eb42db19236fc720ce54f6c4
muhamadisro27 <mohammadisro2710@gmail.com>
2022-04-18 21:45:54 +0800

    feat : setup resource controller match string

---
+++ app/Http/Controllers/MatchStrController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MatchStr;
+use Illuminate\Http\Request;
+
+class MatchStrController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MatchStr  $matchStr
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MatchStr $matchStr)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MatchStr  $matchStr
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MatchStr $matchStr)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MatchStr  $matchStr
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MatchStr $matchStr)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MatchStr  $matchStr
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MatchStr $matchStr)
+    {
+        //
+    }
+}
