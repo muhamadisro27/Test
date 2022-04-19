2491a67974fdfdf027d40f8d032eb7bc0030caca
muhamadisro27 <mohammadisro2710@gmail.com>
2022-04-19 19:53:49 +0800

    feat : setup static function match string

--- app/Models/MatchString.php
+++ app/Models/MatchString.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MatchStr extends Model
+class MatchString extends Model
 {
     use HasFactory;
 
@@ -14,4 +14,9 @@ class MatchStr extends Model
         'text',
         'total'
     ];
+
+    public static function matchString()
+    {
+        echo 'Hello';
+    }
 }
