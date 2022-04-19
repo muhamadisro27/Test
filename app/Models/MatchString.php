8a2614def6c94df26c966e5da7982d503fc75a82
muhamadisro27 <mohammadisro2710@gmail.com>
2022-04-19 22:00:33 +0800

    feat : make a logic match string

--- app/Models/MatchString.php
+++ app/Models/MatchString.php
@@ -17,6 +17,39 @@ class MatchString extends Model
 
     public static function matchString()
     {
-        echo 'Hello';
+            // Ambil request dari inputan
+            $pattern = request('match');
+            $text = request('text');
+   
+
+            // buat string menjadi huruf kecil
+            $pattern = strtolower($pattern);
+            $text = strtolower($text);
+            
+
+            // split string menjadi array
+            $pattern = str_split($pattern);
+            $text = str_split($text);
+            
+            // default jumlah data match
+            $find_match = 0;
+
+            // jumlah dari array
+            $lp = count($pattern);
+            $lt = count($text);
+          
+            // lakukan perulangan pada dan pencocokan string
+            for($i=0; $i < $lt; $i++) {
+                for($j=0; $j < $lp; $j++) {
+                    if ($pattern[$j] != $text[$i]) {
+                        break;
+                    } else {
+                        $find_match++;
+                    }
+                }
+            }            
+            $result = round(($find_match / $lp)) * 100;
+
+            return $result;
     }
 }
